613e6abb11acf3c6f2ca9bd46a64287da2bc1664
Banana2528 <jaybaker608@gmail.com>
2025-06-18 14:58:48 -0400

    Create smartload.js

---
+++ smartload.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  window.smartlook||(function(d) {
+    var o=smartlook=function(){ o.api.push(arguments)},h=d.getElementsByTagName('head')[0];
+    var c=d.createElement('script');o.api=new Array();c.async=true;c.type='text/javascript';
+    c.charset='utf-8';c.src='https://web-sdk.smartlook.com/recorder.js';h.appendChild(c);
+    })(document);
+    smartlook('init', '7ac199bfafe56028509e5bfbd8c31416570d340c');
+}, 7500);
