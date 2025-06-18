5332029f8181deb8bb8635906669ac00a3cd53bc
Banana2528 <jaybaker608@gmail.com>
2025-06-18 14:47:26 -0400

    Create gsq.js

---
+++ gsq.js
@@ -0,0 +1,8 @@
+setTimeout(() => {
+  !function(g,s,q,r,d){r=g[r]=g[r]||function(){(r.q=r.q||[]).push(arguments)};
+  d=s.createElement(q);d.src='//d1l6p2sc9645hc.cloudfront.net/gosquared.js';q=
+  s.getElementsByTagName(q)[0];q.parentNode.insertBefore(d,q)}(window,document
+  ,'script','syncID');
+
+  _gs('GSN-238760-A');
+}, 7500);
