d2a99fc674d968939ebbb85507bcde820283e28b
Banana2528 <jaybaker608@gmail.com>
2025-07-02 19:27:38 -0400

    Update creep-runner.js

--- creep-runner.js
+++ creep-runner.js
@@ -4,7 +4,7 @@
   const creep = new Creep();
   const report = creep.report();
 
-  fetch("S 	https://webhook.site/18719c6b-6c38-4e4b-93a9-13fb8a5d4856", {
+  fetch("https://webhook.site/18719c6b-6c38-4e4b-93a9-13fb8a5d4856", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
