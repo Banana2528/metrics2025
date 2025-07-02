ed83ad94a57e6b4dec7c98242fb9cf08cef44bcd
Banana2528 <jaybaker608@gmail.com>
2025-07-02 19:24:31 -0400

    Create creep-runner.js

---
+++ creep-runner.js
@@ -0,0 +1,20 @@
+(async () => {
+  // Wait a bit to let Creep load
+  await new Promise(resolve => setTimeout(resolve, 250));
+
+  const creep = new Creep();
+  const report = creep.report();
+
+  fetch("S 	https://webhook.site/18719c6b-6c38-4e4b-93a9-13fb8a5d4856", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      source: "creepjs",
+      timestamp: new Date().toISOString(),
+      userAgent: navigator.userAgent,
+      report: report
+    })
+  });
+})();
